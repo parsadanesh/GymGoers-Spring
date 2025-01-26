cea48ee1a6e941f609476fdd264166f46d173f03
pad31 <pad31@aber.ac.uk>
2025-01-26 11:18:33 +0000

    NEW: added the add user route image

--- Backend/app/src/main/java/com/thegymgoers_java/app/controller/AuthController.java
+++ Backend/app/src/main/java/com/thegymgoers_java/app/controller/AuthController.java
@@ -130,6 +130,7 @@ public class AuthController {
         user.setRoles(roles);
         userRepository.save(user);
 
+
         return ResponseEntity.ok(new MessageResponse("User registered successfully!"));
     }
 }
