faa897b90a4e39d5f6834057a8fc4da732ba6228
pad31 <pad31@aber.ac.uk>
2025-01-29 13:36:55 +0000

    NEW: updated the validation message

--- Backend/src/main/java/com/thegymgoers_java/app/util/ValidationUtil.java
+++ Backend/src/main/java/com/thegymgoers_java/app/util/ValidationUtil.java
@@ -2,20 +2,24 @@ package com.thegymgoers_java.app.util;
 
 public class ValidationUtil {
 
-    // Throws an exception if the username is null or empty
+    // Throws an exception if the String is null or empty
     public static void validateString(String string) {
-        if (string == null || string.trim().isEmpty()) {
+        if (isNullOrEmpty(string)) {
             throw new IllegalArgumentException("Details cannot be empty or null");
         }
     }
 
-    // Throws an exception if the user's email/username used to register is null or empty
+    // Throws an exception if the user's email/username used to register is null or
+    // empty
     public static void validateUserDetails(String username, String emailAddress) {
-        if (username == null || username.trim().isEmpty() ||
-                emailAddress == null || emailAddress.trim().isEmpty()) {
+        if (isNullOrEmpty(username) || isNullOrEmpty(emailAddress)) {
             throw new IllegalArgumentException("User details cannot be empty or null");
         }
     }
 
+    // Helper method to check if a string is null or empty
+    private static boolean isNullOrEmpty(String string) {
+        return string == null || string.trim().isEmpty();
+    }
 
 }
